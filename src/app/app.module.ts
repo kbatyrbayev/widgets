8d774e05248bf2209a95ad02c6e948764ed426c0
Kairat Batyrbayev <k.batyrbayev>
2022-09-12 16:37:55 +0600

    fix styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,19 @@ import { AppComponent } from './app.component';
 import { Widget1Component } from './widgets/widget1/widget1.component';
 import { WidgetHeaderComponent } from './components/widget-header/widget-header.component';
 import { Widget2Component } from './widgets/widget2/widget2.component';
+import { Widget3Component } from './widgets/widget3/widget3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Widget1Component,
     WidgetHeaderComponent,
-    Widget2Component
+    Widget2Component,
+    Widget3Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
