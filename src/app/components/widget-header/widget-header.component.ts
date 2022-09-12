314627cb6a9e7e0abb0e4c327a6494cc8d981976
Kairat Batyrbayev <k.batyrbayev>
2022-09-12 15:05:57 +0600

    widget 1, map of Kazakhstan

---
+++ src/app/components/widget-header/widget-header.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-widget-header',
+  templateUrl: './widget-header.component.html',
+  styleUrls: ['./widget-header.component.scss']
+})
+export class WidgetHeaderComponent implements OnInit {
+
+  @Input() name = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
